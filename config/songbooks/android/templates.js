2fee17b2af2856947bc4c9e55f2f91cc614d794a
SAM SOLOMON PRABU SD <samsolomonprabu@users.noreply.github.com>
2023-05-12 12:14:18 +0530

    Adspace changed

---
+++ config/songbooks/android/templates.js
@@ -0,0 +1,27 @@
+/* Tamil */
+var template = {
+    "ad": {
+        "title": "Tamil Bible",
+        "description": "The Ultimate Tamil Bible Reader - Tamil Bible app includes Compare Bible, Audio Bible, Photo Editor, Bible Dictionary, Daily Verse, Wallpapers, Bible Quiz, Articles, Books History, Bible History, Bible Sermons, Notes, Bookmarks, 1000 praises and so many exciting features. ",
+        "link": "https://play.google.com/store/apps/details?id=sam.bible.tamilfree",
+        "buttonText": "Download",
+        "buttonIcon": ""
+    },
+    "ad": {
+        "title": "Support Us",
+        "description": "Help us keep running",
+        "link": "https://play.google.com/store/apps/details?id=sam.bible.tamilfree",
+        "buttonText": "Download",
+        "buttonIcon": ""
+    }
+};
+
+
+/* Telugu */
+"ad": {
+    "title": "Telugu Bible",
+    "description": "The Ultimate Telugu Bible Reader - Telugu Bible app includes Compare Bible, Audio Bible, Photo Editor, Bible Dictionary, Daily Verse, Wallpapers, Bible Quiz, Articles, Books History, Bible History, Bible Sermons, Notes, Bookmarks, 1000 praises and so many exciting features.",
+    "link": "https://play.google.com/store/apps/details?id=sam.bible.telugufree",
+    "buttonText": "Download",
+    "buttonIcon": ""
+}
